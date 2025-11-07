161aad6047b3b4536cc4f7cf95e27c15193e274a
astrxnomo <luisgir827@gmail.com>
2025-11-06 22:25:50 -0500

    Fix: Calendar event user icon

--- components/calendar/core/event/event-item.tsx
+++ components/calendar/core/event/event-item.tsx
@@ -10,6 +10,7 @@ import {
   MapPinIcon,
   School,
   University,
+  User,
   UserCircle,
 } from "lucide-react"
 
@@ -302,7 +303,7 @@ export function EventItem({
               <span className="truncate">{event.title}</span>
             </span>
             {event?.created_by?.user_id === profile?.user_id && (
-              <UserCircle className="h-3.5 w-3.5 shrink-0 opacity-70" />
+              <User className="h-3 w-3 shrink-0 opacity-70" />
             )}
           </span>
         )}
